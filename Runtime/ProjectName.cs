97369fac12cdac717210801921232432c54779a8
mrstruijk <mrstruijk@gmail.com>
2024-09-18 15:59:14 +0200

    Auto formatting

--- Runtime/ProjectName.cs
+++ Runtime/ProjectName.cs
@@ -11,8 +11,6 @@ namespace SOSXR.ClonedProjectHelpers
             {
                 var fullPath = Application.dataPath;
 
-                Debug.Log(nameof(FullPath) + " Full Path is " + fullPath);
-
                 return fullPath;
             }
         }
@@ -23,8 +21,6 @@ namespace SOSXR.ClonedProjectHelpers
             var split = FullPath.Split('/');
 
             var projectName = split[^2];
-
-            Debug.LogFormat(nameof(ProjectName) + " Project Name is " + projectName);
 
             return projectName;
         }
